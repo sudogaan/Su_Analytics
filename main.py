1fa538d35116e55be3d144577510856a19ea21ce
sudogaan <sudoogaan@gmail.com>
2022-05-07 12:38:11 +0300

    update

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+lines = '|\n'
+coffee=f"""
+{'-'*40}
+{lines*5}Su's Morning Coffee Updates {lines*5}
+{'-'*40}
+"""
+print(coffee)
+
+from datetime import date
+  
+today = date.today()
+print("Date:", today)
+
+
